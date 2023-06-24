cbf7f648e162507c6409298dd3b4fe775efdfd14
Tuğçe KANER <118681134+Tugcekaner@users.noreply.github.com>
2023-06-24 11:02:42 +0300

    .

--- user/views.py
+++ user/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from .models import *
 from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.models import User
-# from django.contrib import messages
+from django.contrib import messages
 # from django.db.models import Count
 
 # Create your views here.
@@ -13,7 +13,7 @@ from django.contrib.auth.models import User
 #     if request.method == "POST":
 #         username = request.POST.get("username")
 #         password = request.POST.get("password")
-      
+
 #     user = authenticate(username=username, password=password) # bulamazsa None
 
 #     context = {}
@@ -26,26 +26,61 @@ def loginPage(request):
     if request.method == "POST":
         username = request.POST.get("username")
         password = request.POST.get("password")
-      
-        user = authenticate(username=username, password=password)  # bulamazsa None
+
+        user = authenticate(username=username,password=password)
 
         context = {}
 
         if user is not None:
             login(request, user)
-            context.update({"profile": User.objects.get(username=request.user.username)})
+            context.update({"profile": UserInfo.objects.get(username=request.user.username)})
             return redirect('index')
         else:
-            # Kullanıcı kimlik doğrulaması başarısız oldu
             context["login_error"] = "Geçersiz kullanıcı adı veya şifre."
 
-    return render(request, 'login.html')
+    context = {}
+    if request.user.is_authenticated:
+        context.update({"profile": UserInfo.objects.get(user=request.user)})
 
-
+    return render(request, 'login.html',context)
 
 
 def registerPage(request):
-    return render(request,'register.html')
+
+    if request.method == "POST":
+        username = request.POST.get("username")
+        email = request.POST.get("email")
+        password1 = request.POST.get("password1")
+        password2 = request.POST.get("password2")
+
+        if password1 == password2:
+            if not User.objects.filter(username=username).exists():
+                if not User.objects.filter(email=email).exists():
+                    user = User.objects.create_user(
+                        username=username, email=email)
+
+                    # userinfo = UserInfo(user=user)
+                    # userinfo.save()
+                    messages.success(request, 'Kaydınız başarıyla oluşturuldu..')
+                    return redirect("login")
+                else:
+                    messages.warning(request, 'Bu mail zaten kullanılıyor!!')
+                    return redirect("login")
+            else:
+                messages.warning(
+                    request, 'Bu kullanıcı adı daha önceden alınmış!!')
+                return redirect("login")
+        else:
+            messages.warning(request, 'Şifreler aynı değil!!')
+            return redirect("register")
+
+    context = {
+        'user':user,
+    }
+
+    if request.user.is_authenticated:
+        context.update({"profile": UserInfo.objects.get(user=request.user)})
+    return render(request,'register.html',context)
 
 def logoutUser(request):
    logout(request)
